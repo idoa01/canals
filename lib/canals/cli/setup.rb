058982007105f2c2938630b46c56f55c0c6e6cc4
Ido Abramovich <ido@kontera.com>
2016-02-04 12:13:07 +0200

    refractor wizard and add 'setup completion'

--- lib/canals/cli/setup.rb
+++ lib/canals/cli/setup.rb
@@ -1,6 +1,5 @@
 require 'canals'
 require 'canals/tools/completion'
-require 'colorize'
 require 'thor'
 
 module Canals
@@ -10,24 +9,36 @@ module Canals
 
       desc "wizard", "Run the setup wizard"
       def wizard
-        say "Welcome to the Canals Setup Wizard!".green
+        say "Welcome to the Canals Setup Wizard!", :green
+        check_install_completion
+        say "Setup complete. Thanks! :)", :green
+      end
+
+      desc "completion", "Setup bash completion"
+      def completion
         install_completion
-        say "Setup complete. Thanks! :)".green
       end
 
       no_commands do
+        def check_install_completion
+          if !check(Canals::Tools::Completion.completion_installed?, "Checking if shell completion is installed...")
+            yes?('Shell completion not installed. Would you like to install it now? ') && install_completion
+          end
+        end
+
         def install_completion
-          say "Checking if shell completion is installed... "
-          if Canals::Tools::Completion.completion_installed?
-            say "yes".green  and return
-          else
-            say "no".red
-          end
-          install = yes?('Shell completion not installed. Would you like to install it now? ')
-          if install
-            Canals::Tools::Completion.install_completion
-            puts "Shell completion installed.".green
-          end
+          Canals::Tools::Completion.install_completion
+          say "Shell completion installed.", :green
+        end
+
+        def check(check_result, message)
+          spaces = "  " * (shell.padding + 1)
+          say "#{spaces}#{message}#{spaces}#{humanize(check_result)}"
+          return check_result
+        end
+
+        def humanize(bool)
+          bool ? set_color("yes",:green) : set_color("no", :red)
         end
       end
 
