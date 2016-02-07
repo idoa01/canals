830050dd3402d56f9e9af0ce8d23d5c61479f6c7
Ido Abramovich <ido@kontera.com>
2016-02-07 12:47:42 +0200

    make default more noticeable

--- lib/canals/cli/environment.rb
+++ lib/canals/cli/environment.rb
@@ -32,8 +32,8 @@ module Canals
           return
         end
         require 'terminal-table'
-        rows = Canals.environments.select{ |e| env.nil? || e.name == env}.map{ |e| [e.name + (e.is_default? ? "*" : ""), e.user, e.hostname, e.pem] }
-        table = Terminal::Table.new :headings => ['Name', 'User', 'Hostname', 'PEM'], :rows => rows
+        rows = Canals.environments.select{ |e| env.nil? || e.name == env}.map{ |e| [(e.is_default? ? "*" : ""), e.name, e.user, e.hostname, e.pem] }
+        table = Terminal::Table.new :headings => ['', 'Name', 'User', 'Hostname', 'PEM'], :rows => rows
         say table
       end
 
